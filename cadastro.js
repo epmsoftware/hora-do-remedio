7b3e1ce92ee997f436dfc0513603321ff1082f16
edson melo <epm.tes@gmail.com>
2025-05-03 20:14:27 -0300

    página cadastro de medicamentos

--- cadastro.js
+++ cadastro.js
@@ -1,4 +1,4 @@
-document.getElementById('medicamentoForm').addEventListener('submit', function(event) {
+document.getElementById('medicamentoForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Impede o envio padrão do formulário
 
     const nome = document.getElementById('nome').value;
@@ -23,15 +23,15 @@ document.getElementById('medicamentoForm').addEventListener('submit', function(e
         },
         body: JSON.stringify({ nome, validade, quantidade, frequencia, dosagem, frequencia1horario1, frequencia2horario1, frequencia2horario2, frequencia3horario1, frequencia3horario2, frequencia3horario3, descricao })
     })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.message); // Exibe uma mensagem de sucesso
-        document.getElementById('medicamentoForm').reset(); // Limpa o formulário
-    })
-    .catch(error => console.error('Erro:', error));
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message); // Exibe uma mensagem de sucesso
+            document.getElementById('medicamentoForm').reset(); // Limpa o formulário
+        })
+        .catch(error => console.error('Erro:', error));
 });
 
-document.getElementById('frequencia').addEventListener('click', function() {
+document.getElementById('frequencia').addEventListener('click', function () {
     var container1horario = document.getElementById('container1horario');
     var dosagem = document.getElementById('containerDosagem');
     if (this.value === 'Uma vez ao dia') {
@@ -42,7 +42,7 @@ document.getElementById('frequencia').addEventListener('click', function() {
     }
 });
 
-document.getElementById('frequencia').addEventListener('click', function() {
+document.getElementById('frequencia').addEventListener('click', function () {
     var container2horario = document.getElementById('container2horario');
     var dosagem2 = document.getElementById('containerDosagem');
     if (this.value === 'Duas vezes ao dia') {
@@ -53,7 +53,7 @@ document.getElementById('frequencia').addEventListener('click', function() {
     }
 });
 
-document.getElementById('frequencia').addEventListener('click', function() {
+document.getElementById('frequencia').addEventListener('click', function () {
     var container3horario = document.getElementById('container3horario');
     var dosagem3 = document.getElementById('containerDosagem');
     if (this.value === 'Três vezes ao dia') {
@@ -64,7 +64,7 @@ document.getElementById('frequencia').addEventListener('click', function() {
     }
 });
 
-document.getElementById('frequencia').addEventListener('click', function() {
+document.getElementById('frequencia').addEventListener('click', function () {
     var personalizado = document.getElementById('personalizado');
     if (this.value === 'Personalizado') {
         personalizado.style.display = 'block';
