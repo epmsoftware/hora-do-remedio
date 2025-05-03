651b750215d78482dcffc315578ba509f17ba9c7
edson melo <epm.tes@gmail.com>
2025-05-03 19:06:25 -0300

    página cadastro de medicamentos

--- cadastro.js
+++ cadastro.js
@@ -4,8 +4,14 @@ document.getElementById('medicamentoForm').addEventListener('submit', function(e
     const nome = document.getElementById('nome').value;
     const validade = document.getElementById('validade').value;
     const quantidade = document.getElementById('quantidade').value;
-    const horario1 = document.getElementById('horario1').value;
-    const horario2 = document.getElementById('horario2').value;
+    const frequencia = document.getElementById('frequencia').value;
+    const dosagem = document.getElementById('dosagem').value;
+    const frequencia1horario1 = document.getElementById('frequencia1horario1').value;
+    const frequencia2horario1 = document.getElementById('frequencia2horario1').value;
+    const frequencia2horario2 = document.getElementById('frequencia2horario2').value;
+    const frequencia3horario1 = document.getElementById('frequencia3horario1').value;
+    const frequencia3horario2 = document.getElementById('frequencia3horario2').value;
+    const frequencia3horario3 = document.getElementById('frequencia3horario3').value;
     const descricao = document.getElementById('descricao').value;
 
 
@@ -15,7 +21,7 @@ document.getElementById('medicamentoForm').addEventListener('submit', function(e
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ nome, validade, quantidade, horario1, horario2, descricao })
+        body: JSON.stringify({ nome, validade, quantidade, frequencia, dosagem, frequencia1horario1, frequencia2horario1, frequencia2horario2, frequencia3horario1, frequencia3horario2, frequencia3horario3, descricao })
     })
     .then(response => response.json())
     .then(data => {
@@ -23,4 +29,46 @@ document.getElementById('medicamentoForm').addEventListener('submit', function(e
         document.getElementById('medicamentoForm').reset(); // Limpa o formulário
     })
     .catch(error => console.error('Erro:', error));
+});
+
+document.getElementById('frequencia').addEventListener('click', function() {
+    var container1horario = document.getElementById('container1horario');
+    var dosagem = document.getElementById('containerDosagem');
+    if (this.value === 'Uma vez ao dia') {
+        dosagem.style.display = 'block';
+        container1horario.style.display = 'block';
+    } else {
+        container1horario.style.display = 'none';
+    }
+});
+
+document.getElementById('frequencia').addEventListener('click', function() {
+    var container2horario = document.getElementById('container2horario');
+    var dosagem2 = document.getElementById('containerDosagem');
+    if (this.value === 'Duas vezes ao dia') {
+        dosagem2.style.display = 'block';
+        container2horario.style.display = 'block';
+    } else {
+        container2horario.style.display = 'none';
+    }
+});
+
+document.getElementById('frequencia').addEventListener('click', function() {
+    var container3horario = document.getElementById('container3horario');
+    var dosagem3 = document.getElementById('containerDosagem');
+    if (this.value === 'Três vezes ao dia') {
+        dosagem3.style.display = 'block';
+        container3horario.style.display = 'block';
+    } else {
+        container3horario.style.display = 'none';
+    }
+});
+
+document.getElementById('frequencia').addEventListener('click', function() {
+    var personalizado = document.getElementById('personalizado');
+    if (this.value === 'Personalizado') {
+        personalizado.style.display = 'block';
+    } else {
+        personalizado.style.display = 'none';
+    }
 });
